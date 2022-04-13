2c7d138921b18098034481cfd780bcc7328fe5c9
Oshane Bailey <b4.oshany@gmail.com>
2022-04-13 16:46:31 +0000

    Initial commit

---
+++ src/NotificationSettingsServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Osoobe\NotificationSettings;
+
+use Illuminate\Support\ServiceProvider;
+
+class NotificationSettingsServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
